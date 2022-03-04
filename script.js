8e6436de591864978cb2d8927fc48c5f79a5a549
unknown <kikridu@gmail.com>
2022-03-04 00:54:14 -0700

    moving local storage

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 var today = moment().format("MMMM Do, YYYY - hh:mm a");
 var now = moment();
-
+var abtn = document.getElementById("buttn9")
+var thearea = document.getElementById("9AM")
 $("#currentDay").text(today);
+
+
+abtn.addEventListener('click', function() {
+    
+    var thisthing = $('#9AM').val()
+    console.log(thisthing);
+
+    localStorage.setItem('item', thisthing)
+
+
+});
 
 
 
@@ -12,10 +24,9 @@ var audit9AM = function () {
 
     var time = moment(now, "L").set("hour", 9);
     var limit = moment(now, "L").set("hour", 10);
-    
+
     if (moment().isAfter(limit)) {
         $("#9AM").addClass("past");
-        console.log('true')
     } else if (moment().isBefore(time)) {
         $("#9AM").addClass("future")
     } else if (moment().isBefore(limit)) {
@@ -31,10 +42,10 @@ var audit10AM = function () {
 
     var time = moment(now, "L").set("hour", 10);
     var limit = moment(now, "L").set("hour", 11);
-    
+
     if (moment().isAfter(limit)) {
         $("#10AM").addClass("past");
-        console.log('true')
+
     } else if (moment().isBefore(time)) {
         $("#10AM").addClass("future")
     } else if (moment().isBefore(limit)) {
@@ -50,10 +61,10 @@ var audit11AM = function () {
 
     var time = moment(now, "L").set("hour", 11);
     var limit = moment(now, "L").set("hour", 12);
-    
+
     if (moment().isAfter(limit)) {
         $("#11AM").addClass("past");
-        console.log('true')
+
     } else if (moment().isBefore(time)) {
         $("#11AM").addClass("future")
     } else if (moment().isBefore(limit)) {
@@ -69,10 +80,10 @@ var audit12PM = function () {
 
     var time = moment(now, "L").set("hour", 12);
     var limit = moment(now, "L").set("hour", 13);
-    
+
     if (moment().isAfter(limit)) {
         $("#12PM").addClass("past");
-        console.log('true')
+
     } else if (moment().isBefore(time)) {
         $("#12PM").addClass("future")
     } else if (moment().isBefore(limit)) {
@@ -88,10 +99,10 @@ var audit1PM = function () {
 
     var time = moment(now, "L").set("hour", 13);
     var limit = moment(now, "L").set("hour", 14);
-    
+
     if (moment().isAfter(limit)) {
         $("#1PM").addClass("past");
-        console.log('true')
+
     } else if (moment().isBefore(time)) {
         $("#1PM").addClass("future")
     } else if (moment().isBefore(limit)) {
@@ -107,10 +118,10 @@ var audit2PM = function () {
 
     var time = moment(now, "L").set("hour", 14);
     var limit = moment(now, "L").set("hour", 15);
-    
+
     if (moment().isAfter(limit)) {
         $("#2PM").addClass("past");
-        console.log('true')
+
     } else if (moment().isBefore(time)) {
         $("#2PM").addClass("future")
     } else if (moment().isBefore(limit)) {
@@ -126,10 +137,10 @@ var audit3PM = function () {
 
     var time = moment(now, "L").set("hour", 15);
     var limit = moment(now, "L").set("hour", 16);
-    
+
     if (moment().isAfter(limit)) {
         $("#3PM").addClass("past");
-        console.log('true')
+
     } else if (moment().isBefore(time)) {
         $("#3PM").addClass("future")
     } else if (moment().isBefore(limit)) {
@@ -145,10 +156,10 @@ var audit4PM = function () {
 
     var time = moment(now, "L").set("hour", 16);
     var limit = moment(now, "L").set("hour", 17);
-    
+
     if (moment().isAfter(limit)) {
         $("#4PM").addClass("past");
-        console.log('true')
+
     } else if (moment().isBefore(time)) {
         $("#4PM").addClass("future")
     } else if (moment().isBefore(limit)) {
@@ -164,10 +175,10 @@ var audit5PM = function () {
 
     var time = moment(now, "L").set("hour", 17);
     var limit = moment(now, "L").set("hour", 18);
-    
+
     if (moment().isAfter(limit)) {
         $("#5PM").addClass("past");
-        console.log('true')
+
     } else if (moment().isBefore(time)) {
         $("#5PM").addClass("future")
     } else if (moment().isBefore(limit)) {
